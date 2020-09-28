3fdd1f1c055581842af67b78952c0e3e169a2da2
Gianluca Palermo <gianluca.palermo@polimi.it>
2020-09-28 12:55:18 +0200

    initial version 2021

---
+++ 2021/sources/generate.sh
@@ -0,0 +1,7 @@
+cat head.html.src index.html.src footer.html.src > ../index.html
+cat head.html.src cfp.html.src footer.html.src > ../cfp.html
+cat head.html.src keynote.html.src footer.html.src > ../keynote.html	
+cat head.html.src previous.html.src footer.html.src > ../previous.html		
+cat head.html.src reg.html.src footer.html.src > ../reg.html		
+cat head.html.src tpc.html.src footer.html.src > ../tpc.html		
+cat head.html.src program.html.src footer.html.src > ../program.html		
