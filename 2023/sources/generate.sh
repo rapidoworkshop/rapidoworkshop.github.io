43ad24e29758f2d3e82f6148cc99b1e5a1c2a7d7
gianlucapalermo <32489026+gianlucapalermo@users.noreply.github.com>
2023-01-09 09:32:52 +0100

    Updated program

--- 2023/sources/generate.sh
+++ 2023/sources/generate.sh
@@ -4,4 +4,4 @@ cat head.html.src cfp.html.src footer.html.src > ../cfp.html
 cat head.html.src previous.html.src footer.html.src > ../previous.html		
 cat head.html.src reg.html.src footer.html.src > ../reg.html		
 cat head.html.src tpc.html.src footer.html.src > ../tpc.html		
-#cat head.html.src program.html.src footer.html.src > ../program.html		
+cat head.html.src program.html.src footer.html.src > ../program.html		
